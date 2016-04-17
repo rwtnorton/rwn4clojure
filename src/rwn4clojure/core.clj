0101a628c1434ab874d47dad467aa90b299db59d
Richard W. Norton <rwtnorton@gmail.com>
2016-04-16 22:54:54 -0700

    p128

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -921,6 +921,41 @@
 (deftest t126
   (is (p126 java.lang.Class)))
 
+;; A standard American deck of playing cards has four suits - spades,
+;; hearts, diamonds, and clubs - and thirteen cards in each suit. Two
+;; is the lowest rank, followed by other integers up to ten; then the
+;; jack, queen, king, and ace.
+;;
+;; It's convenient for humans to represent these cards as suit/rank
+;; pairs, such as H5 or DQ: the heart five and diamond queen respectively.
+;; But these forms are not convenient for programmers, so to write a
+;; card game you need some way to parse an input string into meaningful
+;; components. For purposes of determining rank, we will define the
+;; cards to be valued from 0 (the two) to 12 (the ace)
+;;
+;; Write a function which converts (for example) the string "SJ" into
+;; a map of {:suit :spade, :rank 9}. A ten will always be represented
+;; with the single character "T", rather than the two characters "10".
+(defn p128 [__]
+  [(= {:suit :diamond :rank 10} (__ "DQ"))
+   (= {:suit :heart :rank 3} (__ "H5"))
+   (= {:suit :club :rank 12} (__ "CA"))
+   (= (range 13) (map (comp :rank __ str)
+                   '[S2 S3 S4 S5 S6 S7
+                     S8 S9 ST SJ SQ SK SA]))])
+(deftest t128
+  (let [f (fn [[s r]]
+            (let [char->suit {\D :diamond
+                              \H :heart
+                              \C :club
+                              \S :spade}
+                  char->rank (->> "23456789TJQKA"
+                                  (map-indexed (comp vec reverse vector))
+                                  (into {}))]
+              {:suit (char->suit s)
+               :rank (char->rank r)}))]
+    (is (all? (p128 f)))))
+
 ;; Write a function which, given a key and map, returns true
 ;; iff the map contains an entry with that key and its value is nil.
 (defn p134 [__]
