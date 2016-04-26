9011a480c737667962a207d4b0a24416d595bd5e
Richard W. Norton <rwtnorton@gmail.com>
2016-04-25 20:41:04 -0700

    p59

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -610,6 +610,20 @@
                    first)))]
     (is (all? (p58 h)))))
 
+;; Take a set of functions and return a new function that takes a
+;; variable number of arguments and returns a sequence containing the
+;; result of applying each function left-to-right to the argument list.
+(defn p59 [__]
+  [(= [21 6 1] ((__ + max min) 2 3 5 1 6 4))
+   (= ["HELLO" 5] ((__ #(.toUpperCase %) count) "hello"))
+   (= [2 6 4] ((__ :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))])
+(deftest t59
+  (let [f (fn [& fns]
+            (fn [& vs]
+              (for [g fns]
+                (apply g vs))))]
+    (is (all? (p59 f)))))
+
 (defn p61 [__]
   [(= (__ [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})
    (= (__ [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})
