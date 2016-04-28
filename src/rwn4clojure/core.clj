fa06aeabb15da6bcb598eb1829075bf2c64853a0
Richard W. Norton <rwtnorton@gmail.com>
2016-04-28 00:54:07 -0700

    p65

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -666,6 +666,50 @@
 (deftest t64
   (is (all? (p64 +))))
 
+;; Clojure has many sequence types, which act in subtly different ways.
+;; The core functions typically convert them into a uniform "sequence"
+;; type and work with them that way, but it can be important to
+;; understand the behavioral and performance differences so that you
+;; know which kind is appropriate for your application.
+
+;; Write a function which takes a collection and returns one of :map,
+;; :set, :list, or :vector - describing the type of collection it was
+;; given.
+;; You won't be allowed to inspect their class or use the built-in
+;; predicates like list? - the point is to poke at them and understand
+;; their behavior.
+(defn p65 [__]
+  [(= :map (__ {:a 1, :b 2}))
+   (= :list (__ (range (rand-int 20))))
+   (= :vector (__ [1 2 3 4 5 6]))
+   (= :set (__ #{10 (rand-int 5)}))
+   (= [:map :set :vector :list] (map __ [{} #{} [] ()]))])
+(deftest t65
+  (let [f (fn [coll]
+            (case (->> coll empty str)
+              "{}" :map
+              "#{}" :set
+              "[]" :vector
+              "()" :list
+              :dunno))
+        g (fn [coll]
+            (let [z (empty coll)]
+              (cond
+                (= 1 (count (conj z [1 1] [1 2])))
+                :map
+
+                (= 1 (count (conj z [1 1] [1 1])))
+                :set
+
+                (= [1 1] (first (conj z [1 1] [2 2])))
+                :vector
+
+                (= [2 2] (first (conj z [1 1] [2 2])))
+                :list
+
+                :else :dunno)))]
+    (is (all? (p65 f)))))
+
 (defn p66 [__]
   [(= (__ 2 4) 2)
    (= (__ 10 5) 5)
