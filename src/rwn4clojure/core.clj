fad6d09f75ead71631817a3307828459aedd8611
Richard W. Norton <rwtnorton@gmail.com>
2016-04-29 22:03:49 -0700

    p80

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -845,6 +845,32 @@
                  set))]
     (is (all? (p77 f)))))
 
+;; A number is "perfect" if the sum of its divisors equal the number
+;; itself. 6 is a perfect number because 1+2+3=6. Write a function
+;; which returns true for perfect numbers and false otherwise.
+(defn p80 [__]
+  [(= (__ 6) true)
+   (= (__ 7) false)
+   (= (__ 496) true)
+   (= (__ 500) false)
+   (= (__ 8128) true)])
+(deftest t80
+  (let [f (fn [n]
+            (->> n
+                 Math/sqrt
+                 int
+                 range
+                 (map inc)
+                 (filter (fn [d] (zero? (rem n d))) )
+                 (map (fn [v] [v (quot n v)]))
+                 rest
+                 flatten
+                 (into #{})
+                 (apply +)
+                 inc
+                 (= n)))]
+    (is (all? (p80 f)))))
+
 (defn p81 [__]
   [(= (__ #{0 1 2 3} #{2 3 4 5}) #{2 3})
    (= (__ #{0 1 2} #{3 4 5}) #{})
