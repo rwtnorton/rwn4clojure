7b01e910a39ec213720d7524a75c9ccb6f6d6f8e
Richard W. Norton <rwtnorton@gmail.com>
2016-05-07 16:47:15 -0700

    p110

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -1336,6 +1336,35 @@
               (reduce * (repeat exponent base))))]
     (is (all? (p107 f)))))
 
+;; Write a function that returns a lazy sequence of "pronunciations"
+;; of a sequence of numbers. A pronunciation of each element in the
+;; sequence consists of the number of repeating identical numbers and
+;; the number itself. For example, [1 1] is pronounced as [2 1] ("two
+;; ones"), which in turn is pronounced as [1 2 1 1] ("one two, one
+;; one").
+;;
+;; Your function should accept an initial sequence of numbers, and
+;; return an infinite lazy sequence of pronunciations, each element
+;; being a pronunciation of the previous element.
+(defn p110 [__]
+  [(= [[1 1] [2 1] [1 2 1 1]] (take 3 (__ [1])))
+   (= [3 1 2 4] (first (__ [1 1 1 4 4])))
+   (= [1 1 1 3 2 1 3 2 1 1] (nth (__ [1]) 6))
+   (= 338 (count (nth (__ [3 2]) 15)))])
+(deftest t110
+  (let [pron (fn [vs]
+               (loop [vs vs, acc []]
+                 (if (seq vs)
+                   (let [v (first vs)
+                         cnt (->> vs (take-while #(= v %)) count)]
+                     (recur (drop cnt vs) (conj acc cnt v)))
+                   acc)))
+        f (fn [vs]
+            (->> vs
+                 (iterate pron)
+                 rest))]
+    (is (all? (p110 f)))))
+
 ;; A balanced number is one whose component digits have the same sum
 ;; on the left and right halves of the number. Write a function which
 ;; accepts an integer n, and returns true iff n is balanced.
