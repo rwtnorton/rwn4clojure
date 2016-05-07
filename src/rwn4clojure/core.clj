f0fb15e79fac0bbe066b3d28b4d7c2421f4d1e44
Richard W. Norton <rwtnorton@gmail.com>
2016-05-07 16:17:37 -0700

    p137

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -1521,6 +1521,33 @@
                     (->> (rest vs) (partition 2))))]
     (is (all? (p135 f)))))
 
+;; Write a function which returns a sequence of digits of a non-negative
+;; number (first argument) in numerical system with an arbitrary base
+;; (second argument). Digits should be represented with their integer
+;; values, e.g. 15 would be [1 5] in base 10, [1 1 1 1] in base 2 and
+;; [15] in base 16.
+(defn p137 [__]
+  [(= [1 2 3 4 5 0 1] (__ 1234501 10))
+   (= [0] (__ 0 11))
+   (= [1 0 0 1] (__ 9 2))
+   (= [1 0] (let [n (rand-int 100000)](__ n n)))
+   (= [16 18 5 24 15 1] (__ Integer/MAX_VALUE 42))])
+(deftest t137
+  (let [f (fn [n b]
+            {:pre [((complement neg?) n)
+                   (<= 2 b)]}
+            (if (zero? n)
+              [0]
+              (rseq
+               (loop [x n, acc []]
+                 (if (zero? x)
+                   acc
+                   (let [q (quot x b)
+                         r (rem x b)
+                         acc' (conj acc r)]
+                     (recur q acc')))))))]
+    (is (all? (p137 f)))))
+
 (defn p143 [__]
   [(= 0 (__ [0 1 0] [1 0 0]))
    (= 3 (__ [1 1 1] [1 1 1]))
