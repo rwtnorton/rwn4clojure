ae9aca08cc5d1f5aefa759716387cbdde2d3f3f8
Richard W. Norton <rwtnorton@gmail.com>
2016-05-08 16:03:32 -0700

    p114

--- src/rwn4clojure/core.clj
+++ src/rwn4clojure/core.clj
@@ -1394,6 +1394,34 @@
                  rest))]
     (is (all? (p110 f)))))
 
+;; take-while is great for filtering sequences, but it limited: you
+;; can only examine a single item of the sequence at a time. What if
+;; you need to keep track of some state as you go over the sequence?
+;;
+;; Write a function which accepts an integer n, a predicate p, and a
+;; sequence. It should return a lazy sequence of items in the list up
+;; to, but not including, the nth item that satisfies the predicate.
+(defn p114 [__]
+  [(= [2 3 5 7 11 13]
+      (__ 4 #(= 2 (mod % 3))
+          [2 3 5 7 11 13 17 19 23]))
+   (= ["this" "is" "a" "sentence"]
+      (__ 3 #(some #{\i} %)
+          ["this" "is" "a" "sentence" "i" "wrote"]))
+   (= ["this" "is"]
+      (__ 1 #{"a"}
+          ["this" "is" "a" "sentence" "i" "wrote"]))])
+(deftest t114
+  (let [f (fn [n p vs]
+            (->> (let [cnt (atom n)]
+                   (map (fn [v]
+                          (let [orly (p v)]
+                            [v orly (if orly (swap! cnt dec) @cnt)]))
+                        vs))
+                 (filter (fn [[_ _ x]] (pos? x)))
+                 (map first)))]
+    (is (all? (p114 f)))))
+
 ;; A balanced number is one whose component digits have the same sum
 ;; on the left and right halves of the number. Write a function which
 ;; accepts an integer n, and returns true iff n is balanced.
